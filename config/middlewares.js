58a7d30512180b48fa8a6e4bcdf02ef1f5dcbd88
Vinicius Oliveira <vinicius.oliveira@local>
2025-07-25 06:47:31 -0300

    feat: storage aws

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,9 @@
+const { env } = require('@strapi/utils');
+
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +11,32 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
+
+
