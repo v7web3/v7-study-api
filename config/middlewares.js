8a49abd4ac7c0320b755758ec8053d0e16fbec42
Vinicius Oliveira <vinicius.oliveira@local>
2025-07-25 09:31:59 -0300

    build

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,7 +3,7 @@ const { env } = require('@strapi/utils');
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -11,32 +11,32 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  // {
-  //   name: 'strapi::security',
-  //   config: {
-  //     contentSecurityPolicy: {
-  //       useDefaults: true,
-  //       directives: {
-  //         'connect-src': ["'self'", 'https:'],
-  //         'img-src': [
-  //           "'self'",
-  //           'data:',
-  //           'blob:',
-  //           'dl.airtable.com',
-  //           `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
-  //         ],
-  //         'media-src': [
-  //           "'self'",
-  //           'data:',
-  //           'blob:',
-  //           'dl.airtable.com',
-  //           `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
-  //         ],
-  //         upgradeInsecureRequests: null,
-  //       },
-  //     },
-  //   },
-  // },
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `${env("AWS_BUCKET")}.s3.${env("AWS_REGION")}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
 
 
--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,9 @@
 module.exports = ({env}) => ({
     email: {
         config: {
-          provider: 'strapi-provider-mailersend', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+          provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
           providerOptions: {
-            apiKey: env('MAILERSEND_API_KEY'),
+            apiKey: env('SENDGRID_API_KEY'),
           },
           settings: {
             defaultFrom: 'no-reply@v7web.com.br',
@@ -12,24 +12,24 @@ module.exports = ({env}) => ({
           },
         },
       },
-    //   upload: {
-    //     config: {
-    //       provider: 'aws-s3',
-    //       providerOptions: {
-    //         accessKeyId: env('AWS_ACCESS_KEY_ID'),
-    //         secretAccessKey: env('AWS_ACCESS_SECRET'),
-    //         region: env('AWS_REGION'),
-    //         params: {
-    //           ACL: env('AWS_ACL', 'public-read'),
-    //           signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-    //           Bucket: env('AWS_BUCKET'),
-    //         },
-    //       },
-    //       actionOptions: {
-    //         upload: {},
-    //         uploadStream: {},
-    //         delete: {},
-    //       },
-    //     },
-    //   },
+      upload: {
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
+            params: {
+              ACL: env('AWS_ACL', 'public-read'),
+              signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+              Bucket: env('AWS_BUCKET'),
+            },
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      },
 });
