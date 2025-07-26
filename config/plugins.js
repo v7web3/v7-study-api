2cdb199613d053d9769eeb9b35feaa10d794d706
Vinicius Oliveira <vinicius.oliveira@local>
2025-07-26 11:27:22 -0300

    feat: implements refreshToken

--- config/plugins.js
+++ config/plugins.js
@@ -44,5 +44,25 @@ module.exports = ({env}) => {
         },
       },
     },
+    'users-permissions': {
+        config: {
+          jwt: {
+            /* the following  parameter will be used to generate:
+             - regular tokens with username and password
+             - refreshed tokens when using the refreshToken API
+            */
+            expiresIn: '24h', // This value should be lower than the refreshTokenExpiresIn below.
+          },
+        },
+    },
+  'refresh-token': {
+    config: {
+      refreshTokenExpiresIn: '60d', // this value should be higher than the jwt.expiresIn
+      requestRefreshOnAll: false, // automatically send a refresh token in all login requests.
+      refreshTokenSecret: env('REFRESH_JWT_SECRET') || 'SomethingSecret',
+      cookieResponse: false, // if set to true, the refresh token will be sent in a cookie
+      refreshTokenRotation: false, // forces a new Refresh token, deleting the previously used one from the db.
+    },
+  },
   };
 };
